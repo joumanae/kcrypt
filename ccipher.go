946653959f1e951987783973be4f31d862fa3ae6
Joumana El Alaoui <joumana.codes@gmail.com>
2023-02-17 15:19:32 -0800

    Fixes the red herring

--- ccipher.go
+++ ccipher.go
@@ -13,7 +13,7 @@ func (c *Cipher) Encipher(s string) string {
 	var b bytes.Buffer
 	var ciphered string
 	for _, r := range s {
-		r = ShiftRune(r, 1)
+		r = ShiftRune(r, c.Key)
 		b.WriteRune(r)
 		ciphered = b.String()
 	}
@@ -26,8 +26,14 @@ func ShiftRune(r rune, Shift int) rune {
 	} else {
 		r = unicode.ToUpper(r)
 	}
-	if r+rune(Shift) >= 'Z' {
+	if r+rune(Shift) >= 'Z' && Shift != 13 {
 		return r + rune(Shift) - 26
+	}
+	if r > 'M' && Shift == 13 {
+		return r - 13
+	}
+	if r == 'M' && Shift == 13 {
+		return 'Z'
 	}
 	return r + rune(Shift)
 }
@@ -36,7 +42,7 @@ func (c *Cipher) Decipher(s string) string {
 	var deciphered string
 	var b bytes.Buffer
 	for _, r := range s {
-		r = ShiftRune(r, -1)
+		r = ShiftRune(r, -c.Key)
 		b.WriteRune(r)
 		deciphered = b.String()
 	}
--- cmd/main.go
+++ cmd/main.go
@@ -9,13 +9,19 @@ import (
 func main() {
 	var cipherText, decipherText bool
 	var message string
+	var key int
 	flag.BoolVar(&cipherText, "ct", false, "What needs to be encrypted")
 	flag.BoolVar(&decipherText, "dt", false, "What needs to be decrypted")
 	flag.StringVar(&message, "m", " ", "This is the message to cipher or decipher")
+	flag.IntVar(&key, "k", 1, "This is the key to cipher or decipher")
 	flag.Parse()
 
-	c := ccypher.Cipher{Key: 1}
-	d := ccypher.Cipher{Key: -1}
-	fmt.Println(c.Encipher(message))
-	fmt.Println(d.Decipher(message))
+	if cipherText {
+		c := ccypher.Cipher{Key: key}
+		fmt.Println(c.Encipher(message))
+	}
+	if decipherText {
+		d := ccypher.Cipher{Key: -key}
+		fmt.Println(d.Decipher(message))
+	}
 }
