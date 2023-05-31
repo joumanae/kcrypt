4cfa8397dc749f200ac5067e845e6102cce4c653
Joumana El Alaoui <joumana.codes@gmail.com>
2023-05-31 13:27:41 -0700

    Adds a Fuzz test for the SharedKey function

--- dhkeygen/dhkeygen_test.go
+++ dhkeygen/dhkeygen_test.go
@@ -13,17 +13,6 @@ func FuzzTestPublicKey(f *testing.F) {
 	})
 }
 
-func FuzzTestSharedKey(f *testing.F) {
-	f.Fuzz(func(t *testing.T, modulus int, base int, secret int) {
-		PK, err := dhkeygen.PublicKey(base, modulus)
-		if err != nil {
-			t.Skip()
-		}
-		dhkeygen.SharedKey(PK, secret, modulus)
-
-	})
-}
-
 func TestParseBigInt(t *testing.T) {
 
 	got, ok := dhkeygen.ParseBigInt("52")
@@ -53,4 +42,14 @@ func TestGenerateSecretKey(t *testing.T) {
 	if t1.Cmp(t2) != 0 {
 		t.Errorf("the two users do not have the same shared key: want %v, got %v", t1, t2)
 	}
+}
+
+func FuzzTestSharedKey(f *testing.F) {
+	f.Fuzz(func(t *testing.T, modulus int, base int, secret int) {
+		secret1 := dhkeygen.SharedKey(big.NewInt(int64(base)), secret, modulus)
+		secret2 := dhkeygen.SharedKey(big.NewInt(int64(base)), secret, modulus)
+		if secret1.Cmp(secret2) != 0 {
+			t.Errorf("the two users do not have the same shared key: want %v, got %v", secret1, secret2)
+		}
+	})
 }
