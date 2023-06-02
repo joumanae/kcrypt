26a3e168de835b67fe2adef84a56901bb013410f
Joumana El Alaoui <joumana.codes@gmail.com>
2023-06-02 15:47:04 -0700

    Reverting to older code

--- dhkeygen/dhkeygen_test.go
+++ dhkeygen/dhkeygen_test.go
@@ -13,6 +13,32 @@ func FuzzTestPublicKey(f *testing.F) {
 	})
 }
 
+func FuzzTestSharedKey(f *testing.F) {
+	f.Fuzz(func(t *testing.T, modulus int, base int, secret int) {
+
+		pk1, err1 := dhkeygen.PublicKey(base, modulus)
+		if err1 != nil {
+			return
+		}
+		pk2, err2 := dhkeygen.PublicKey(base, modulus)
+		if err2 != nil {
+			return
+		}
+		secret1 := dhkeygen.GenerateSecretKey()
+		secret2 := dhkeygen.GenerateSecretKey()
+		key1 := dhkeygen.SharedKey(pk2, secret1, modulus)
+		if modulus == 0 && base == 00 {
+			t.Skip()
+		}
+
+		key2 := dhkeygen.SharedKey(pk1, secret2, modulus)
+
+		if key1.Cmp(key2) != 0 {
+			t.Errorf("the two users do not have the same shared key: key 1: %v, key 2: %v", key1, key2)
+		}
+	})
+}
+
 func TestParseBigInt(t *testing.T) {
 
 	got, ok := dhkeygen.ParseBigInt("52")
@@ -24,32 +50,4 @@ func TestParseBigInt(t *testing.T) {
 	if got.Cmp(want) != 0 {
 		t.Errorf("want %v, got %v", want, got)
 	}
-}
-
-func TestGenerateSecretKey(t *testing.T) {
-	secret1 := dhkeygen.GenerateSecretKey()
-	secret2 := dhkeygen.GenerateSecretKey()
-	pk1, err := dhkeygen.PublicKey(2, 5)
-	if err != nil {
-		t.Fatal(err)
-	}
-	pk2, err := dhkeygen.PublicKey(2, 5)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t1 := dhkeygen.SharedKey(big.NewInt(pk1.Int64()), secret1, 5)
-	t2 := dhkeygen.SharedKey(big.NewInt(pk2.Int64()), secret2, 5)
-	if t1.Cmp(t2) != 0 {
-		t.Errorf("the two users do not have the same shared key: want %v, got %v", t1, t2)
-	}
-}
-
-func FuzzTestSharedKey(f *testing.F) {
-	f.Fuzz(func(t *testing.T, modulus int, base int, secret int) {
-		secret1 := dhkeygen.SharedKey(big.NewInt(int64(base)), secret, modulus)
-		secret2 := dhkeygen.SharedKey(big.NewInt(int64(base)), secret, modulus)
-		if secret1.Cmp(secret2) != 0 {
-			t.Errorf("the two users do not have the same shared key: want %v, got %v", secret1, secret2)
-		}
-	})
 }
